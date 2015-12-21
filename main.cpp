ad04d273e1a56c3b77fa485aef8443228f2ec93a
Davide Botti <davidebotti@hotmail.it>
2015-12-21 11:58:57 +0100

    [files works] .wav files works only for 16bit pcm
    tested the files, they works but they should be converted to
    16 bit pcm.
    
    Signed-off-by: Davide Botti <davidebotti@hotmail.it>

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include "player.h"
 #include "sad_trombone.h"
 #include "la.h"
-#include"do1.h"
-
+#include"fa1.h"
+#include"do2.h"
 /*
 	scaricare audacity. 
 	convertire in un header il wav.istruzioni nel convert.cpp (terraneo l'ha testato con noi).
@@ -24,7 +24,9 @@ int main()
 	//passa le note
 	ADPCMSound sound(sad_trombone_bin,sad_trombone_bin_len);
 	ADPCMSound la_sound(la_bin,la_bin_len);
-	ADPCMSound do_sound(do1_bin,do1_bin_len);
+	ADPCMSound do_sound(do2_bin,do2_bin_len);
+	ADPCMSound fa_sound(fa1_bin,fa1_bin_len);
+	
 	/*
 		instanziare diversi oggetti ognuno con una nota diversa
 		e riprodurli 
@@ -36,8 +38,8 @@ int main()
 	int i;
 	Player::instance().init();
 	for(i=0;i<10;++i) {
+		Player::instance().play(fa_sound);
 		Player::instance().play(do_sound);
-		Player::instance().play(la_sound);
 	} 
 	Player::instance().stop();
 
