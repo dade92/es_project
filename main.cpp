0fcf1e79ea971ddb5d50e2c9091461d5947c82b9
Davide Botti <davidebotti@hotmail.it>
2015-12-22 09:56:24 +0100

    [test with serial] serial port tested
    Now in the main function it gets a character from the serial
    and do a simple parsing: different chars produce different notes.
    
    Signed-off-by: Davide Botti <davidebotti@hotmail.it>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
-
+/*
+	actually I don't know which of these
+	I need in order to use scanf.
+	For the serial: 
+	red->VDD
+	black->GND
+	yellow->PB10
+	orange->PB11
+*/
 #include "player.h"
 #include "sad_trombone.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <cstdio>
+#include <cstring>
+#include <list>
+#include <stdexcept>
+#include <miosix.h>
 #include "la.h"
 #include"fa1.h"
 #include"do2.h"
@@ -18,15 +34,16 @@
 	nel buffer ci sono livelli di tensione,e  li costruisce piano piano
 	
 */
+ADPCMSound sound(sad_trombone_bin,sad_trombone_bin_len);
+ADPCMSound la_sound(la_bin,la_bin_len);
+ADPCMSound do_sound(do2_bin,do2_bin_len);
+ADPCMSound fa_sound(fa1_bin,fa1_bin_len);
+void play_sound(char c);
 int main()
 {
 	//codifica molto semplice, lossy, codifica un 4 bit ogni nota
 	//passa le note
-	ADPCMSound sound(sad_trombone_bin,sad_trombone_bin_len);
-	ADPCMSound la_sound(la_bin,la_bin_len);
-	ADPCMSound do_sound(do2_bin,do2_bin_len);
-	ADPCMSound fa_sound(fa1_bin,fa1_bin_len);
-	
+
 	/*
 		instanziare diversi oggetti ognuno con una nota diversa
 		e riprodurli 
@@ -37,10 +54,29 @@ int main()
 	*/
 	int i;
 	Player::instance().init();
-	for(i=0;i<10;++i) {
-		Player::instance().play(fa_sound);
-		Player::instance().play(do_sound);
-	} 
+	for(;;) 
+		play_sound(getchar());
 	Player::instance().stop();
 
+}
+/*
+	simple function that reproduce some notes based
+	on their name (C=DO,D=RE,A=la,F=fa ecc..)
+	this function at the end should be something
+	like play_sound(int[] byte_stream) and I should
+	pass to it a piece of the MIDI file. Then I parse
+	it.
+*/
+void play_sound(char c) {
+	switch(c) {
+		case 'a':
+			Player::instance().play(la_sound);
+			break;
+		case 'c':
+			Player::instance().play(fa_sound);
+			break;
+		case 'f':
+			Player::instance().play(do_sound);
+			break;
+	}
 }
