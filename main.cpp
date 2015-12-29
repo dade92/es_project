b52cd34da2520808aa36dd5d1e0cee03b83cc9cf
Davide Botti <davidebotti@hotmail.it>
2015-12-29 09:40:06 +0100

    [Added notes] added the notes
    Added the notes in the form of APCDMSound objects; now we can
    play music passing char to the serial line.
    
    Signed-off-by: Davide Botti <davidebotti@hotmail.it>

--- main.cpp
+++ main.cpp
@@ -20,6 +20,16 @@
 #include"02do2#.h"
 #include"01do2.h"
 #include"08sol2.h"
+#include"03re2.h"
+#include"04re2#.h"
+#include"05mi2.h"
+#include"06fa2.h"
+#include"07fa2#.h"
+#include"09sol2#.h"
+#include"10la2.h"
+#include"11la2#.h"
+#include"12si2.h"
+#include"13do3.h"
 /*
 	scaricare audacity. 
 	convertire in un header il wav.istruzioni nel convert.cpp (terraneo l'ha testato con noi).
@@ -35,8 +45,20 @@
 	
 */
 ADPCMSound sound(sad_trombone_bin,sad_trombone_bin_len);
-ADPCMSound do_sound(__01do2_bin,__01do2_bin_len);
-ADPCMSound dod_sound(__02do2__bin,__02do2__bin_len);
+ADPCMSound do2_sound(__01do2_bin,__01do2_bin_len);
+ADPCMSound do2d_sound(__02do2__bin,__02do2__bin_len);
+ADPCMSound re2_sound(__03re2_bin,__03re2_bin_len);
+ADPCMSound re2d_sound(__04re2__bin,__04re2__bin_len);
+ADPCMSound mi2_sound(__05mi2_bin,__05mi2_bin_len);
+ADPCMSound fa2_sound(__06fa2_bin,__06fa2_bin_len);
+ADPCMSound fa2d_sound(__07fa2__bin,__07fa2__bin_len);
+ADPCMSound sol2_sound(__08sol2_bin,__08sol2_bin_len);
+ADPCMSound sol2d_sound(__09sol2__bin,__09sol2__bin_len);
+ADPCMSound la2_sound(__10la2_bin,__10la2_bin_len);
+ADPCMSound la2d_sound(__11la2__bin,__11la2__bin_len);
+ADPCMSound si2_sound(__12si2_bin,__12si2_bin_len);
+ADPCMSound do3_sound(__13do3_bin,__13do3_bin_len);
+
 void play_sound(char c);
 int main()
 {
@@ -51,13 +73,19 @@ int main()
 		generare .h di un suono.
 		modificare 	
 	*/
-	int i;
 	Player::instance().init();
+	/*
 	for(;;) {
-		Player::instance().play(do_sound);
-		Player::instance().play(dod_sound);
-	}
-		
+		Player::instance().play(do2_sound);
+		Player::instance().play(re2_sound);
+		Player::instance().play(mi2_sound);
+		Player::instance().play(fa2_sound);
+		Player::instance().play(sol2_sound);
+		Player::instance().play(la2_sound);
+		Player::instance().play(si2_sound);
+		Player::instance().play(do3_sound);
+	}*/
+	for(;;) play_sound(getchar());
 	Player::instance().stop();
 
 }
@@ -69,16 +97,32 @@ int main()
 	pass to it a piece of the MIDI file. Then I parse
 	it.
 */
+/*usage=use the second line of the qwerty:a=do(C). s=re(D). d=mi(E). etc..*/
 void play_sound(char c) {
 	switch(c) {
 		case 'a':
-		
+			Player::instance().play(do2_sound);
 			break;
-		case 'c':
-			Player::instance().play(do_sound);
+		case 's':
+			Player::instance().play(re2_sound);
+			break;
+		case 'd':
+			Player::instance().play(mi2_sound);
 			break;
 		case 'f':
-		
+			Player::instance().play(fa2_sound);
+			break;
+		case 'g':
+			Player::instance().play(sol2_sound);
+			break;
+		case 'h':
+			Player::instance().play(la2_sound);
+			break;
+		case 'j':
+			Player::instance().play(si2_sound);
+			break;
+		case 'k':
+			Player::instance().play(do3_sound);
 			break;
 	}
 }
